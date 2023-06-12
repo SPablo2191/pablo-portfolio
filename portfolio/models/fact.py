b13a3fe1529ebf7aa1e6c195a21450ce04599cb3
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-12 07:19:50 -0500

    feat: format fact

--- portfolio/models/fact.py
+++ portfolio/models/fact.py
@@ -1,7 +1,8 @@
 from django.db import models
 from .user import User
+
+
 class Fact(models.Model):
     description = models.TextField(max_length=255)
     title = models.TextField(max_length=100)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
