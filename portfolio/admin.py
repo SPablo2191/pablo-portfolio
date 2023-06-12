290aa5853f64d7c6c408c90fe0dd33fdb73703a3
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-12 08:10:24 -0500

    feat: django rest framework added

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models.achievement import Achievement
+from .models.skill import Skill
+from .models.study import Study
+from .models.project import Project,TechnologyProject
+from .models.technology import Technology
+from .models.company import Company
+from .models.fact import Fact
+from .models.user import User
+from .models.company import Company
 # Register your models here.
