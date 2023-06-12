a41963f89c0f04c252dc3855d5a0250eac4b71a3
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-12 08:22:31 -0500

    feat: admin module working

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,11 +1,13 @@
-from django.contrib import admin
+# from django.contrib import admin
 from .models.achievement import Achievement
-from .models.skill import Skill
-from .models.study import Study
-from .models.project import Project,TechnologyProject
-from .models.technology import Technology
 from .models.company import Company
 from .models.fact import Fact
+from .models.project import Project, TechnologyProject
+from .models.role import Role
+from .models.skill import Skill, TechnologyExperience
+from .models.study import Study
+from .models.technology import Technology
 from .models.user import User
-from .models.company import Company
+from .models.work_experience import WorkExperience
+
 # Register your models here.
--- portfolio/models/user.py
+++ portfolio/models/user.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib import admin
+
 
 class User(models.Model):
     username = models.TextField(max_length=20)
     password = models.CharField(max_length=20)
+
     def __str__(self):
         return self.username
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "password")
