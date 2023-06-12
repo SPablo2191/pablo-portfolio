eb41e6966d7c1e27f9f2f0cc90a5ed35a8a45517
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-12 07:19:00 -0500

    feat: str method in User

--- portfolio/models/user.py
+++ portfolio/models/user.py
@@ -3,4 +3,5 @@ from django.db import models
 class User(models.Model):
     username = models.TextField(max_length=20)
     password = models.CharField(max_length=20)
-
+    def __str__(self):
+        return self.username
