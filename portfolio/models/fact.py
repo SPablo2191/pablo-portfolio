37b778fb8baa25659eeadf087cda5900262f8f7f
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-12 07:25:20 -0500

    fix: change order of fields in fact model

--- portfolio/models/fact.py
+++ portfolio/models/fact.py
@@ -3,6 +3,6 @@ from .user import User
 
 
 class Fact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=255)
     title = models.TextField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
