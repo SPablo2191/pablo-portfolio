14817d2531fd102a4c528f73c8c77f55812618c7
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-12 07:16:13 -0500

    feat: Fact model added

---
+++ portfolio/models/fact.py
@@ -0,0 +1,7 @@
+from django.db import models
+from .user import User
+class Fact(models.Model):
+    description = models.TextField(max_length=255)
+    title = models.TextField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
