1e5a0c6bfea5197f85436d6e4c36d95a347c5838
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-14 10:52:10 -0500

    feat: company serializer

--- portfolio/viewsets/company_view.py
+++ portfolio/viewsets/company_view.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from ..models.company import Company,CompanySerializer
-
+from ..models.company import Company
+from ..serializers.company_serializer import CompanySerializer
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
