5a11e4b58ecbd007576059eb7d3c2a4e6a5756d4
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-06-14 10:24:16 -0500

    feat: str function in role added

--- portfolio/models/role.py
+++ portfolio/models/role.py
@@ -3,14 +3,19 @@ from .user import User
 from django.contrib import admin
 from rest_framework import serializers
 
+
 class Role(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.TextField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
     list_display = ("user", "name")
+
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
