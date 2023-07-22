6c4f8828b7759e7393d4f91889c55191c20c3a83
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-07-22 12:44:23 -0300

    fix: image url in facts

--- portfolio/models/fact.py
+++ portfolio/models/fact.py
@@ -8,11 +8,11 @@ class Fact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=400)
     title = models.TextField(max_length=100)
-
+    image_url = models.CharField(max_length=255,default='')
 
 @admin.register(Fact)
 class FactAdmin(admin.ModelAdmin):
-    list_display = ("user", "title", "description")
+    list_display = ("user", "title", "description","image_url")
 
 
 
